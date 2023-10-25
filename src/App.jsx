c010c203f4096bd45d2f2b8799b959007f43e919
Baccarani Pham <baccaranip@gmail.com>
2023-10-24 20:05:04 -0400

    Troubleshooting build error

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
   AuthContext,
 } from './utils/app_context'
 import { Protected } from './utils/router_utils'
-import './app.css'
+// import './app.css'
 
 export default function App() {
   const [quizData, setQuizData] = useState('')
