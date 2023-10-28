d34091c1a23b634668c1f72eb5fbe7117cee765b
Baccarani Pham <baccaranip@gmail.com>
2023-10-27 22:48:42 -0400

    Sending bomb question from jeopardy page to quiz page

--- src/components/QuizPage/QuizPage.jsx
+++ src/components/QuizPage/QuizPage.jsx
@@ -17,6 +17,7 @@ const QuizPage = () => {
 
   useEffect(() => {
     console.log(selectedQuestion)
+    console.log('isBomb:', selectedQuestion.isBomb)
   }, [selectedQuestion])
 
   const handleAnswerClick = (answer) => {
@@ -55,7 +56,7 @@ const QuizPage = () => {
 
   return (
     <div>
-      <h1 className='text-center py-6 font-bold text-3xl text-white'>Personalized Trivia</h1>
+      <h1 className='text-center py-6 font-bold text-3xl text-white'>Spooky Trivia</h1>
       <div className='bg-black py-1'>
         <div className='flex flex-col justify-center text-white'>
           <h2 className='text-center'>{selectedQuestion.category}</h2>
@@ -63,9 +64,13 @@ const QuizPage = () => {
         </div>
       </div>
       <div className='flex justify-center quiz-page'>
-        <div style={{width: 632}}>
-          <h2 className='text-center font-bold text-2xl px-4 py-6 text-white quiz-container'>{selectedQuestion.questionData.Question}</h2>
-          <div className="flex flex-col gap-4 px-4 flex-grow">
+        <div style={{ width: 632 }}>
+          <h2 className='text-center font-bold text-2xl px-4 py-6 text-white quiz-container'>
+            {selectedQuestion.isBomb ? (
+              <span className="text-red-500">BOMB QUESTION: </span>
+            ) : null}
+            {selectedQuestion.questionData.Question}
+          </h2>          <div className="flex flex-col gap-4 px-4 flex-grow">
             {selectedQuestion.questionData.MultipleChoice.map((answer, index) => (
               <button key={index} className={`bg-gray-100 p-2 rounded-lg flex justify-center items-center h-16 ${selectedAnswer === answer && selected ? 'bg-blue-200' : ''} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={() => handleAnswerClick(answer)} disabled={disabled}>
                 <div className="text-center">{answer}</div>
@@ -76,7 +81,7 @@ const QuizPage = () => {
         {message && (
           <div className="fixed bottom-0 w-full flex flex-col items-center px-4 py-8">
             <p className="text-xl pb-20 text-white">{message}</p>
-            <button className={`bg-green-700 text-white p-2 rounded-lg flex justify-center items-center h-16 text-black font-bold w-full`} style={{maxWidth: 600}} onClick={handleBackClick}>
+            <button className={`bg-green-700 text-white p-2 rounded-lg flex justify-center items-center h-16 text-black font-bold w-full`} style={{ maxWidth: 600 }} onClick={handleBackClick}>
               BACK TO TRIVIA BOARD
             </button>
           </div>
